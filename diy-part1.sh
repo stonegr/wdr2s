64d72e5984e3808eae2ff1b026aa9593b5a8a026
stonegr <74632994+stonegr@users.noreply.github.com>
2021-04-04 14:09:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,14 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone https://github.com/fw876/helloworld.git package/helloworld
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+rm -rf feeds/packages/net/smartdns & git clone https://github.com/pymumu/smartdns.git package/smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/luci-app-koolproxyR
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/stonegr/luci-app-eqos.git package/luci-app-eqos
