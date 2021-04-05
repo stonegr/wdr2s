ae25876fdc8d2dc14cc840503a1385cd8a4b32e3
stonegr <74632994+stonegr@users.noreply.github.com>
2021-04-05 18:45:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.6.180/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/root::0:0:99999:7:::/root:$1$ZzLkZmEb$Kpkpyxaj6bYCEtrs7LqWs.:18612:0:99999:7:::/g' package/base-files/files/etc/shadow
 rm -rf feeds/packages/net/smartdns
 sed -i '12s/eth0/eth1/g' package/base-files/files/etc/board.d/99-default_network
