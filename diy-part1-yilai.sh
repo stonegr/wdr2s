25a7b3201305a515543ccc8e4ffada340cf54de3
stonegr <74632994+stonegr@users.noreply.github.com>
2021-04-21 17:01:19 +0800

    Update diy-part1-yilai.sh

--- diy-part1-yilai.sh
+++ diy-part1-yilai.sh
@@ -15,7 +15,8 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-1.
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 git clone https://github.com/fw876/helloworld.git package/helloworld
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
