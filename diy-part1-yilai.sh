2e4dbe5de590f47a8c88cbf8c443329e3a761807
stonegr <74632994+stonegr@users.noreply.github.com>
2021-04-21 17:00:08 +0800

    Update diy-part1-yilai.sh

--- diy-part1-yilai.sh
+++ diy-part1-yilai.sh
@@ -15,7 +15,9 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 git clone https://github.com/fw876/helloworld.git package/helloworld
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/pymumu/smartdns.git package/smartdns
@@ -27,5 +29,3 @@ git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/stonegr/luci-app-eqos.git package/luci-app-eqos
 #git clone https://github.com/stonegr/small.git package/small
-
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
